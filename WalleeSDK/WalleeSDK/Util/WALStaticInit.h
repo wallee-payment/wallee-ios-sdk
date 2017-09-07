ee916eb834ba39fba8687c588103ab1723dfa3b7
Tobias Ballat <baltob@smoca.ch>
2017-09-07 11:27:09 +0200

    Fix Linter Errors

--- WalleeSDK/WalleeSDK/Util/WALStaticInit.h
+++ WalleeSDK/WalleeSDK/Util/WALStaticInit.h
@@ -6,5 +6,5 @@
 //  Copyright © 2017 smoca AG. All rights reserved.
 //
 
-- (instancetype)init __attribute__((unavailable("You should never initialize this type directly. Use one of the static Initializers")));
-+ (instancetype)new __attribute__((unavailable("You should never initialize this type directly. Use one of the static Initializers")));
+//- (instancetype)init __attribute__((unavailable("You should never initialize this type directly. Use one of the static Initializers")));
+//+ (instancetype)new __attribute__((unavailable("You should never initialize this type directly. Use one of the static Initializers")));
