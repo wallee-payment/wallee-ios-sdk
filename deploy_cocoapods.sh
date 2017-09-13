d02c7ab4b6d257180742fa6801082d0e085c3196
schmidi <schmidi@smoca.ch>
2017-09-13 15:24:20 +0200

    add automated pod deployment

---
+++ deploy_cocoapods.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ -z "${TRAVIS_TAG}" ];
+then
+  echo "[INFO] This is not tagged build. Skipping deployment to cocoapods.";
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ];
+then
+  echo "This is pull request. Skipping deployment to cocoapods."
+else
+  echo "[INFO] Publishing artifacts to cocoapods."
+  pod lib lint
+  pod trunk push
+fi
